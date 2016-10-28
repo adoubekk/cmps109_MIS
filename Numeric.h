61e7911f3e5461e62bea32b976c67965e4831ea4
henry5988 <henry5988@hotmail.com>
2016-10-28 13:42:34 -0700

    Added Type Classes
    Type is the top level ancestor class. Numeric, Real, Char and String are inheriting class. A NumericTest class is included for Numeric unit-testing. Currently, a reference error is known when testing.

---
+++ Numeric.h
@@ -0,0 +1,38 @@
+/*
+Henry Chang
+hechang
+Numeric.h
+*/
+
+#ifndef NUMERIC_H
+#define NUMERIC_H
+#include <string>
+#include "Type.h"
+
+using namespace std;
+
+class Numeric: public Type{
+private:
+	int number;
+	string name;
+public:
+
+    // Constructor-Destructor
+	// Default Constructor
+	Numeric();
+
+	// Parameterized Constructor with name of Numeric and value
+	Numeric(string varName, int value);
+
+	// Destructor
+	~Numeric();
+
+	// Access Methods
+	int getValue();
+
+	// Manipulation Methods
+	void setValue(int value);
+};
+
+#endif // !NUMERIC_H
+
